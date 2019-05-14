73cab4ee89d0a545a103adc1069e44ce29ee7424
Kenneth Portillo <portillokenneth@gmail.com>
2019-05-14 02:37:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,12 +18,10 @@ function sayHiToGrandma(string){
   if (string === string.toLowerCase()){
     return "I can't hear you!"
   }
-}
-
-function sayHiToGrandma(string){
   if (string === string.toUpperCase()){
     return "YES INDEED!"
   }
+  if (string === "I love u, Grandma"){
+    return "I love you, too."
+  }
 }
-
-function sayHiToGrandma
