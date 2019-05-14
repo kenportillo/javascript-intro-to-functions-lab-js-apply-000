1943260cea59818d6c484cf6afd0d90137549873
Kenneth Portillo <portillokenneth@gmail.com>
2019-05-14 02:34:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,11 @@ function sayHiToGrandma(string){
     return "I can't hear you!"
   }
 }
+
+function sayHiToGrandma(string){
+  if (string === string.toUpperCase()){
+    return "YES INDEED!"
+  }
+}
+
+function sayHiToGrandma
